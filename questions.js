4ff2c1f9484487cec6933ed64595c9e740eec9f8
Thuanthantho <86817559+Thuanthantho@users.noreply.github.com>
2022-01-11 00:30:56 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: ' "I have never been to Russia. I think I shall go there next year.” said Bill. ',
+    answer: "Bill said that he had never been to Russia and he thought he would go there the next year.",
+    options: [
+      "Bill said that he had never been to Russia and he thought he would go there the next year.",
+      "Bill said that he would have never been to Russia and he thinks he would go there the next year.",
+      "Bill said that he had never been to Russia and he thinks he will go there the next year.",
+      "Bill said that he has never been to Russia and he thinks he would go there the next year."
+    ]
+  },
+    {
+    numb: 2,
+    question: ' "People believed that Jane retired because of her poor health. ',
+    answer: "Jane was believed to have retired because of her poor health.",
+    options: [
+      "Jane is believed to have retired because of her poor health.",
+      "Jane was believed to have retired because of her poor health.",
+      "It is believed that Jane retired because of her poor health.",
+      "Jane retired because of her poor health was believed."
+    ]
+  },
+    {
+    numb: 3,
+    question: " The government knows the extent of the problem. The government needs to take action soon. ",
+    answer: "Knowing the extent of the problem, the government needs to take action soon.",
+    options: [
+      "The government knows the extent of the problem whereas it needs to take action soon.",
+      "The government knows the extent of the problem so that it needs to take action soon.",
+      "Knowing the extent of the problem, the government needs to take action soon.",
+      "The government knows the extent of the problem, or else it needs to take action soon."
+    ]
+  },
+    {
+    numb: 4,
+    question: " The substance is very toxic. Protective clothing must be worn at all times. ",
+    answer: "So toxic is the substance that protective clothing must be worn at all times.",
+    options: [
+      "Since the substance is very toxic, so protective clothing must be worn at all times.",
+      "So toxic is the substance that protective clothing must be worn at all times.",
+      "The substance is such toxic that protective clothing must be worn at all times.",
+      "The substance is too toxic to wear protective clothing at all times."
+    ]
+  },
+    {
+    numb: 5,
+    question: " John is studying hard. He doesn’t want to fail the exam. ",
+    answer: "John is studying hard in Oder not to fail the next exam",
+    options: [
+      "John is studying hard in Oder not to fail the next exam",
+      "John is studying hard in Oder that he not fail the next exam",
+      "John is studying hard so as to fail the next exam",
+      "John is studying hard in Oder to not to fail the next exam"
+    ]
+  },
+  {
+    numb: 6,
+    question: " She gave in her notice. She planned to start her new job in January ",
+    answer: "She gave in her notice with a view to starting her new job in January",
+    options: [
+      "She gave in her notice, plan to start her new job in January",
+      "She gave in her notice with a view to starting her new job in January",
+      "Her notice was given in with an aim to start her new job in January",
+      "Her notice was given in order for her to start her new job in January."
+    ]
+  },
+  {
+    numb: 7,
+    question: " When the unemployment rate is high, the crime rate is usually also high. ",
+    answer: "The higher the unemployment rate is, the higher the crime rate is.",
+    options: [
+      "The unemployment rate and the crime rate are both higher.",
+      "The higher the unemployment rate is, the higher the crime rate is.",
+      "The unemployment rate is as high as the crime rate.",
+      "The high rate of unemployment depends on the high rate of crime."
+    ]
+  },
+  {
+    numb: 8,
+    question: " I wish you had not said that",
+    answer: "It would be nice if you had not said that.",
+    options: [
+      "I wish you not to say that.",
+      "If only you did not say that",
+      "I hope you will not say that.",
+      "It would be nice if you had not said that."
+    ]
+  },
+  {
+    numb: 9,
+    question: " “You are always making terrible mistakes”, said the teacher. ",
+    answer: "The teacher complained about his students making terrible mistakes.",
+    options: [
+      "The teacher asked his students why they always made terrible mistakes.",
+      "The teacher realized that his students always made terrible mistakes.",
+      "The teacher complained about his students making terrible mistakes.",
+      "The teacher made his students not always make terrible mistakes."
+    ]
+  },
+  {
+    numb: 10,
+    question: " There is no point in your phoning Jane – she’s away. ",
+    answer: "It would be a waste of time phoning Jane – she’s away.",
+    options: [
+      "It would be a waste of time phoning Jane – she’s away.",
+      "You waste your time if you insist on phoning Jane – she’s away.",
+      "Don’t spend your valuable time phoning Jane – she’s out.",
+      "Jane is very difficult to phone – she’s always away."
+    ]
+  },
+];
